3db2c45a2b98012ebdac41d23048e5e35f5f251c
Luis <garcia.lopez.luis109@gmail.com>
2023-06-25 16:04:24 -0600

    proyectos agregados

--- proyectos/proyectos.php
+++ proyectos/proyectos.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -5,12 +8,12 @@
   <!-- Basic -->
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
 
   <title>Proyectos</title>
 
@@ -24,13 +27,35 @@
   <!-- fonts style -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Roboto:400,500&display=swap"
     rel="stylesheet" />
-  <!-- Custom styles for this template -->
   <link href="css/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
+
 </head>
 
 <body>
+<nav id="navbar">
+        <span id="logo">CHASEMOMENTUM</span>
+        <ul>
+            <li><a href="../paginaPrincipal.php" class="navElemento">Home</a></li>
+            <li><a href="../contacto.php" class="navElemento">Contact</a></li>
+            <li> <a href="../about.php" class="navElemento">About</a></li>
+            <li><a href="../blog/index.php" class="navElemento">Blog</a></li>
+            <li><a href="../servicios.php" class="navElemento">Services</a></li>
+            <li><a href="proyectos.php" class="navElemento">Projects</a></li>
+        </ul>
+        <?php
+        if (isset($_SESSION["user"])) {
+            $nombre_usuario = $_SESSION["user"];
+            echo "<ul class=inicios>
+            <li id=nombreUsuario>$nombre_usuario</li>
+            <li><a href=logout.php id=logout>Cerrar Sesion</a></li>
+            </ul>";
+        } else {
+            echo "<ul class='inicios'>
+            <li><a href='login.php' id='login'>Login</a></li>
+            <li><a href='registro.php' id='register'>register</a></li></ul>";
+        }
+        ?>
+    </nav>
 
   <!-- slider section -->
   <section class=" slider_section">
@@ -257,6 +282,17 @@
       }
     });
   </script>
+  <div class="footer">
+        <span id="logo" style="color: #00b4d8;">CHASEMOMENTUM</span>
+        <ul class="iconos">
+            <li>Siguenos</li>
+            <li><a href=""><img src="../iconos/icons8-facebook-25.png" alt=""></a></li>
+            <li><a href=""><img src="../iconos/icons8-twitter-25.png" alt=""></a></li>
+            <li><a href=""><img src="../iconos/icons8-linkedin-25.png" alt=""></a></li>
+            <li><a href=""><img src="../iconos/icons8-youtube-25.png" alt=""></a></li>
+        </ul>
+        <a href="terminos.html" class="terminos">Terminos legales</a>
+    </div>
 </body>
 
 </html>
